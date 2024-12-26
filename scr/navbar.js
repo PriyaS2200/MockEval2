497ca62e48094df469797ac417dfcbb2145c9f69
PriyaS2200 <sonkarpriya667@gmail.com>
2024-12-26 16:48:17 +0530

    MockEval2

---
+++ scr/navbar.js
@@ -0,0 +1,7 @@
+let navbar = `<div id="navbar">
+<a href="index.html"> Home </a>
+<a href="quiz.html"> Quiz </a>
+<a href="questions.html"> Questions </a>
+</div>`
+
+document.getElementById("nav-container").innerHTML = navbar;
---
+++ scr/quiz.js
@@ -0,0 +1,32 @@
+let url = "https://charming-mixolydian-cyclamen.glitch.me/questions";
+
+let form = document.getElementById("quiz-form");
+form.addEventListener("submit", function(){
+    event.preventDefault();
+    let question = document.getElementById("question-statement")
+    let ques = question.value;
+    let optionA = document.getElementById("option-a");
+    let optA = optionA.value;
+    let optionB = document.getElementById("option-b");
+    let optB = optionB.value;
+    let optionC = document.getElementById("option-c");
+    let optC = optionC.value;
+    let optionD = document.getElementById("option-d");
+    let optD = optionD.value;
+    let correctAns = document.getElementById("correctOption");
+    let correctOpt = correctAns.value;
+    let questionsObj = {ques,optA,optB,optC,optD,correctOpt};
+
+    fetch(url,{
+        method: 'POST',
+        headers: {
+            contentType: 'application/json'
+        },
+        body: JSON.stringify(questionsObj)
+    }).then(() => {
+        alert("Question Created")
+    }).catch(err => {
+        alert("Error: " + err.message);
+    })
+
+})
